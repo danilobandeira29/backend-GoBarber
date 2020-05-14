ef00ee5fedabb7d7d38c0ea90edf50dad1ab3421
Danilo Bandeira <danilobandeira29@gmail.com>
2020-05-14 17:53:53 -0300

    add global exception handler/express-async-errors

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -4,19 +4,15 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-	try {
-		const { password, email } = request.body;
+	const { password, email } = request.body;
 
-		const authenticateUser = new AuthenticateUserService();
+	const authenticateUser = new AuthenticateUserService();
 
-		const { user, token } = await authenticateUser.execute({ password, email });
+	const { user, token } = await authenticateUser.execute({ password, email });
 
-		delete user.password;
+	delete user.password;
 
-		return response.json({ user, token });
-	} catch (err) {
-		return response.status(err.statusCode).json({ error: err.message });
-	}
+	return response.json({ user, token });
 });
 
 export default sessionsRouter;
