b27d093e784978712f8e9430964faa8b48f844bd
Danilo Bandeira <danilobandeira29@gmail.com>
2020-08-12 18:04:38 -0300

    feat: add Handlebars to be MailTemplateProvider
    inject MailTemplateProvider in MailProvider because they are directly associated

---
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeTemplateMailProvider.ts
@@ -0,0 +1,11 @@
+import IParseMailTemplateDTO from '../dtos/IParseMailTemplateDTO';
+
+import IMailTemplateProvider from '../models/IMailTemplateProvider';
+
+class FakeTemplateMailProvider implements IMailTemplateProvider {
+	public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
+		return template;
+	}
+}
+
+export default FakeTemplateMailProvider;
